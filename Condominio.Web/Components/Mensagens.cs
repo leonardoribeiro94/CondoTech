fdf8f03711f2a4e151850835bb854a9fda750610
Leonardo Ribeiro da Silva <leonardostudent94@hotmail.com>
2017-10-20 09:26:49 -0200

    Criado componente de exibição de mensagens para web.

---
+++ Condominio.Web/Components/Mensagens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Condominio.Web.Components
+{
+    public class Mensagens
+    {
+        public void MensagemDeExcessao(string excessao, Control nomeDoFormulario)
+        =>
+            ScriptManager.RegisterClientScriptBlock(nomeDoFormulario.Page, GetType(), "modalExcessao",
+                "fn_ModalError(\" " + excessao.Replace("\'", string.Empty)
+                .Replace(Environment.NewLine, string.Empty) + "\");", true);
+    }
+}
