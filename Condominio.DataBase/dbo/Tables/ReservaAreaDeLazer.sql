f1cb226bf7de91fbbfd59b44c825c00070db52bf
Leonardo Ribeiro da Silva <leonardostudent94@hotmail.com>
2017-11-16 13:08:50 -0200

    alteração de campo da tabela de reservas

--- Condominio.DataBase/dbo/Tables/ReservaAreaDeLazer.sql
+++ Condominio.DataBase/dbo/Tables/ReservaAreaDeLazer.sql
@@ -5,7 +5,7 @@
     [Descricao]            VARCHAR (MAX) NULL,
     [DataSolicitacao]      DATETIME      NOT NULL,
     [DataReserva]          DATETIME      NOT NULL,
-    [Status]               INT           NOT NULL,
+    [StatusReserva]               BIT           NOT NULL,
     CONSTRAINT [PK_ReservaAreaDeLazer] PRIMARY KEY CLUSTERED ([IdReservaAreaDeLazer] ASC),
     CONSTRAINT [FK_ReservaAreaDeLazer_AreaDeLazer] FOREIGN KEY ([IdMorador]) REFERENCES [dbo].[AreaDeLazer] ([IdAreaDeLazer]),
     CONSTRAINT [FK_ReservaAreaDeLazer_Morador] FOREIGN KEY ([IdMorador]) REFERENCES [dbo].[Morador] ([IdMorador])
