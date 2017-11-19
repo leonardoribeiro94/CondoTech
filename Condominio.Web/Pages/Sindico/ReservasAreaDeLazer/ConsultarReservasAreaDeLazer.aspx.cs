6bc6725d2a9a3e2349de5ef560ca291e3ba939c2
Leonardo Ribeiro da Silva <leonardostudent94@gmail.com>
2017-11-19 00:33:05 -0200

    inicio consulta de reservas

---
+++ Condominio.Web/Pages/Sindico/ReservasAreaDeLazer/ConsultarReservasAreaDeLazer.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace Condominio.Web.Pages.Sindico.ReservasAreaDeLazer
+{
+    public partial class ConsultarReservasAreaDeLazer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+            }
+        }
+
+        protected void grvReserva_OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void lkbPesquisar_OnClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
